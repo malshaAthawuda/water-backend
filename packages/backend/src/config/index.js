31b70fabe979560b2de61ab3e72eaf025d8a4583
Malsha Athawuda <malshaathawuda2003@gmail.com>
2026-02-26 17:16:12 +0530

    rate limit change

--- packages/backend/src/config/index.js
+++ packages/backend/src/config/index.js
@@ -25,8 +25,8 @@ const config = {
   },
   
   rateLimit: {
-    windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS, 10) || 5 * 1000, // 5 s
-    max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS, 10) || 20,
+    windowMs: parseInt(process.env.RATE_LIMIT_WINDOW_MS, 10) || 60 * 1000, // 5 s
+    max: parseInt(process.env.RATE_LIMIT_MAX_REQUESTS, 10) || 300,
   },
 };
 
